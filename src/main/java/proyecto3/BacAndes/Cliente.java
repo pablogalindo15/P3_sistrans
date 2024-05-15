90a33cf876c9844a091101d87aebcb9d86199dfa
Gabriel Padilla <g.padilla@uniandes.edu.co>
2024-05-15 18:59:37 -0500

    front cliente

--- src/main/java/proyecto3/BacAndes/Cliente.java
+++ src/main/java/proyecto3/BacAndes/Cliente.java
@@ -1,5 +1,4 @@
 package proyecto3.BacAndes;
-
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
--- src/main/java/proyecto3/BacAndes/ClienteController.java
+++ src/main/java/proyecto3/BacAndes/ClienteController.java
@@ -1,33 +1,24 @@
 package proyecto3.BacAndes;
 
-import java.util.List;
-import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
-
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
     @GetMapping
-    public ResponseEntity<List<Cliente>> getAllClientes(){
-        return new ResponseEntity<List<Cliente>>(clienteService.allClientes(),HttpStatus.OK);}
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Cliente>> getSingleCliente(@PathVariable ObjectId id) {
-        return new ResponseEntity<Optional<Cliente>>(clienteService.singleCliente(id),HttpStatus.OK);
+    public ModelAndView listarClientes() {
+        List<Cliente> clientes = clienteService.allClientes();
+        return new ModelAndView("clientes").addObject("clientes", clientes);
     }
-    
 }
-
